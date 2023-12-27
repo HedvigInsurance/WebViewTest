2eae81e42474fec901b65ac8bb467481585e702a
Robin Andeer <robin.andeer@gmail.com>
2023-12-27 17:21:32 +0100

    Add test view

--- WebViewTest/ContentView.swift
+++ WebViewTest/ContentView.swift
@@ -6,14 +6,56 @@
 //
 
 import SwiftUI
+import WebKit
+
+let widgetURL = "https://dev.hedvigit.com/se/widget/run/405471983/fcc30941-3cb8-400a-b271-15056866435a/4a49efd1-729e-4620-a83a-aefdc0f049dd/sign"
+
+class Coordinator: NSObject, WKNavigationDelegate {
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        if let url = navigationAction.request.url, url.pathExtension == "pdf" {
+            if UIApplication.shared.canOpenURL(url) {
+                UIApplication.shared.open(url)
+            }
+        }
+
+        decisionHandler(.allow)
+    }
+}
+
+struct WebView: UIViewRepresentable {
+    let url: URL
+
+    func makeUIView(context: Context) -> WKWebView {
+        let webView = WKWebView()
+        webView.navigationDelegate = context.coordinator
+        return webView
+    }
+
+    func updateUIView(_ webView: WKWebView, context: Context) {
+        webView.load(URLRequest(url: url))
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        return Coordinator()
+    }
+}
 
 struct ContentView: View {
+    @State private var showWebView = false
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button("Show Web View") {
+                showWebView = true
+            }
+            .padding()
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .cornerRadius(10)
+            .sheet(isPresented: $showWebView) {
+                WebView(url: URL(string: widgetURL)!)
+            }
+            
         }
         .padding()
     }
