e6098dc1acc248786069ab804c6bda72b54ba26f
Robin Andeer <robin.andeer@gmail.com>
2023-12-27 14:22:54 +0100

    Initial Commit

---
+++ WebViewTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WebViewTest
+//
+//  Created by Robin Andeer on 2023-12-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WebViewTest/WebViewTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewTestApp.swift
+//  WebViewTest
+//
+//  Created by Robin Andeer on 2023-12-27.
+//
+
+import SwiftUI
+
+@main
+struct WebViewTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
